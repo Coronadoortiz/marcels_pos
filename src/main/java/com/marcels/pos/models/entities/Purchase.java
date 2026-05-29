aa0e70ca1c2b1e04d868079f4a5b3a51bcd502b3
Sebastian Coronado <sebastian.coronado@udea.edu.co>
2026-05-29 08:41:04 -0500

    Update Purchase.java
    Updated purchase entity

--- src/main/java/com/marcels/pos/models/entities/Purchase.java
+++ src/main/java/com/marcels/pos/models/entities/Purchase.java
@@ -1,5 +1,32 @@
 package com.marcels.pos.models.entities;
 
-public class Purchase {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "tbl_purchases")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Purchase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_purchase")
+    private Long idPurchase;
+
+    @Column(name = "date_purchase", nullable = false)
+    private String datePurchase;
+
+    @ManyToOne
+    @JoinColumn(name = "id_provider", nullable = false)
+    private Provider provider;
 }
