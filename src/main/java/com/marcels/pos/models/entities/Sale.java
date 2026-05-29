90a0dc0ce36676c040fe86b95f696f4f9d7237a0
Sebastian Coronado <sebastian.coronado@udea.edu.co>
2026-05-29 08:54:57 -0500

    Update Sale.java
    Updated the Sale entity

--- src/main/java/com/marcels/pos/models/entities/Sale.java
+++ src/main/java/com/marcels/pos/models/entities/Sale.java
@@ -1,5 +1,37 @@
 package com.marcels.pos.models.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_sales")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_sale")
+    private Long idSale;
+
+    @Column(name = "date_sale", nullable = false)
+    private String dateSale;
+
+    @Column(name = "total_amount_sale", nullable = false)
+    private Double totalAmountSale;
+
+    @ManyToOne
+    @JoinColumn(name = "id_payment_method", nullable = false)
+    private PaymentMethod paymentMethod;
 
 }
