6041127a569aacde6e875d53a663e23051e7a2d6
Sebastian Coronado <sebastian.coronado@udea.edu.co>
2026-05-29 08:33:11 -0500

    Update PaymentMethod.java
    Updated the paymentMethod entity

--- src/main/java/com/marcels/pos/models/entities/PaymentMethod.java
+++ src/main/java/com/marcels/pos/models/entities/PaymentMethod.java
@@ -1,5 +1,28 @@
 package com.marcels.pos.models.entities;
 
-public class PaymentMethod {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Table(name = "tbl_payment_methods")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class PaymentMethod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_payment_method")
+    private Long idPaymentMethod;
+
+    @Column(name = "name_payment_method", nullable = false, length = 50)
+    private String namePaymentMethod;
 }
