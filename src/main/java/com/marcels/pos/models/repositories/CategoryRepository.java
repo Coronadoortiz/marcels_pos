883a65056c83604af0be691fe4adcd848faee3e7
Sebastian Coronado <sebastian.coronado@udea.edu.co>
2026-05-29 09:06:55 -0500

    Create CategoryRepository.java
    Created the CategoryRepository file

---
+++ src/main/java/com/marcels/pos/models/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.marcels.pos.models.repositories;
+
+import com.marcels.pos.models.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository 
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
