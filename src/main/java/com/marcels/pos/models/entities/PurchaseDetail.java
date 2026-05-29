73c3e8d4dd2827ec809680aaa3e96b37195c31b6
Sebastian Coronado <sebastian.coronado@udea.edu.co>
2026-05-29 08:48:18 -0500

    Update PurchaseDetail.java
    Updated the purchaseDetail entity

--- src/main/java/com/marcels/pos/models/entities/PurchaseDetail.java
+++ src/main/java/com/marcels/pos/models/entities/PurchaseDetail.java
@@ -1,5 +1,41 @@
 package com.marcels.pos.models.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "purchase_details")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PurchaseDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_purchase_detail")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_purchase", nullable = false)
+    private Long idPurchase;
+
+    @ManyToOne
+    @JoinColumn(name = "id_product", nullable = false)
+    private Long idProduct;
+
+    @Column(name = "amount_purchased")
+    private Integer amountPurchased;
+
+    @Column(name = "purchase_product_price")
+    private Double purchaseProductPrice;
 
 }
