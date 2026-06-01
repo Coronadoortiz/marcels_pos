2de2aabbf27cda8c271bb53dcdb87ad2de05f1ea
Sebastian Coronado <sebastian.coronado@udea.edu.co>
2026-06-01 09:31:33 -0500

    Created purchase and provider controllers
    Created all controllers needed

---
+++ src/main/java/com/marcels/pos/controllers/ProviderController.java
@@ -0,0 +1,48 @@
+package com.marcels.pos.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcels.pos.models.entities.Provider;
+import com.marcels.pos.services.ProviderService;
+
+@RestController
+@RequestMapping("/api/providers")
+public class ProviderController {
+
+    private final ProviderService providerService;
+
+    public ProviderController(ProviderService providerService) {
+        this.providerService = providerService;
+    }
+
+    @GetMapping
+    public List<Provider> getAll() {
+        return providerService.getAllProviders();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Provider> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(providerService.getProviderById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Provider> create(@RequestBody Provider provider) {
+        return new ResponseEntity<>(providerService.saveProvider(provider), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        providerService.deleteProvider(id);
+        return ResponseEntity.noContent().build();
+    }
+}
